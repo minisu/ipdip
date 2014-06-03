1cb08fedd977f6bbd6e01d986733b0aa2736556f
Henrik Olsson <henrik.olsson@smartbear.com>
2014-06-03 16:20:56 +0200

    Implemented InMemoryStorage

--- src/main/java/minisu/ipdip/Decision.java
+++ src/main/java/minisu/ipdip/Decision.java
@@ -1,5 +1,6 @@
 package minisu.ipdip;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.collect.ImmutableList;
 
@@ -8,5 +9,16 @@ import java.util.List;
 public class Decision
 {
 	@JsonProperty
+	String id = "abc";
+
+	@JsonProperty
+	String name = "My decision";
+
+	@JsonProperty
 	List<String> alternatives = ImmutableList.of("Alt1", "Alt2");
+
+	public String getId()
+	{
+		return id;
+	}
 }
--- src/main/java/minisu/ipdip/InMemoryStorage.java
+++ src/main/java/minisu/ipdip/InMemoryStorage.java
@@ -1,19 +1,22 @@
 package minisu.ipdip;
 
-
 import com.google.common.base.Optional;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 public class InMemoryStorage implements DecisionStorage
 {
+	private ConcurrentHashMap<String, Decision> decisions = new ConcurrentHashMap<String, Decision>();
+
 	@Override
 	public void store( Decision decision )
 	{
-
+		decisions.put( decision.getId(), decision );
 	}
 
 	@Override
 	public Optional<Decision> get( String id )
 	{
-		return Optional.of( new Decision() );
+		return Optional.fromNullable( decisions.get( id ) );
 	}
 }
